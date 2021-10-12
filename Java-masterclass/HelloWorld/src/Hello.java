7139d4877bb9f331ddb98d0363b9494af107592b
Tanja <tanjavanalfen@gmail.com>
2021-10-12 11:24:49 +0200

    Hello World project and challenge

---
+++ Java-masterclass/HelloWorld/src/Hello.java
@@ -0,0 +1,11 @@
+public class Hello { 
+	// class keyword defines class
+	// Everything between { }  is class body
+	
+	public static void main(String[] args) {
+		// public is access modifier
+		
+		System.out.println("Hello World");
+		System.out.println("Hello, Me");
+	}
+}
