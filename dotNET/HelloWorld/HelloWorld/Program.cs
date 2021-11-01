246c22c072985e39711ceea889f59429fca28db8
unknown <tanjavanalfen@gmail.com>
2021-11-01 16:34:19 +0100

    Very first C# program!

---
+++ dotNET/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld // Similar to Java package
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Tanja!");
+        }
+    }
+}
